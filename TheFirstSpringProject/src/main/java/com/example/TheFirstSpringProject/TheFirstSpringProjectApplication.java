17fbb432c99d626c471af8d1180414e5d17f97d7
Beshr Hmaid <BeshrHmaid@gmail.com>
2024-07-10 12:10:54 +0300

    first commit

---
+++ TheFirstSpringProject/src/main/java/com/example/TheFirstSpringProject/TheFirstSpringProjectApplication.java
@@ -0,0 +1,16 @@
+package com.example.TheFirstSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFirstSpringProjectApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(TheFirstSpringProjectApplication.class, args);
+
+
+	}
+
+}
---
+++ TheFirstSpringProject/src/test/java/com/example/TheFirstSpringProject/TheFirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TheFirstSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFirstSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
